ba9e21ede869d02d830db0461cbeb7dbd6c06ae8
chauahanjanvi <ck5282647@gmail.com>
2026-01-21 09:29:02 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,67 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#simple interest
+p=1000
+r=5
+t=2
+si=(p * r * t)/100
+print("simple interest=",si)
+simple interest= 100.0
+#print maximum value
+a=26
+b=25
+if a > b:
+    print("Maximum=",a)
+else:
+    print("maximum=",b)
+
+    
+Maximum= 26
+#range
+for i in range (0,6):
+    print(i)
+
+    
+0
+1
+2
+3
+4
+5
+#length
+s=("python")
+print("length=",len(s))
+length= 6
+#print msg
+print("hello world")
+hello world
+#print first character
+s="python"
+print("first character=",s[0])
+first character= p
+#print last character
+>>> s="atmiya"
+>>> print("last character=",s[-1])
+last character= a
+>>> #print positive negative
+>>> n=-5
+>>> if n > 0:
+...     print("positive number")
+... else:
+...     print("negative number")
+... 
+...     
+negative number
+>>> #print sum
+>>> a=37
+>>> b=25
+>>> c=48
+>>> sum=a + b
+>>> sum=a + b + c
+>>> print("sum=",sum)
+sum= 110
+>>> #print name
+>>> name=input("enter your name: ")
+enter your name: janvi
+>>> print("hello",name)
+hello janvi
