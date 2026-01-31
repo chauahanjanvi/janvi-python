5ad053a53d9c0d65374541667e056c6226aa5588
chauahanjanvi <ck5282647@gmail.com>
2026-01-31 09:15:17 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,23 @@
+print("hello world...!!")
+
+x=12
+print(x)
+
+x=21.5
+print(x)
+
+x=1j
+print(x)
+
+x="apple","banana","cherry"
+print(x)
+
+x=True
+print(x)
+
+#output statement
+name=input("Enter your name:")
+print("hello",name)
+
+number=input("Enter Number:")
+print("hello",number)
