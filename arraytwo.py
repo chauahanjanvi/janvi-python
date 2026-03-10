7247043352e40a329ee4f5ca06162d89b439672b
chauahanjanvi <ck5282647@gmail.com>
2026-03-10 08:36:27 +0530

    Add files via upload

---
+++ arraytwo.py
@@ -0,0 +1,60 @@
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+
+print(arr[0]) #first element
+print(arr[2]) #second element
+print(arr[4]) #fifth element
+"""
+
+
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+
+print(arr[-1]) #last element
+print(arr[-2]) #second element
+print(arr[-5]) #first element
+"""
+
+
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
+"""
+
+
+
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4]) #index 1 to 3
+print(arr[:3]) # start to index 2
+print(arr[2:]) #index 2 to end
+print(arr[:]) #entire array
+"""
+
+"""from array import array
+arr=array('i',[10,20,30,40,50,60,70,80])
+print(arr[::2]) #every second element
+print(arr[1::2])#every second element starting from index 1
+print(arr[::3]) #every third element
+"""
+
+
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1]) #from index -4  to -2
+print(arr[-3:])#last three elemnt 
+print(arr[:-2]) #or exepect last two
+"""
+
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,45])
+print(arr)
+"""
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::-1]) #reverse entire array
+
+
