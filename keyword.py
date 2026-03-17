03c9a8072189264337473f74e5aed3dfb1d081f0
chauahanjanvi <ck5282647@gmail.com>
2026-03-17 08:27:06 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,21 @@
+"""def student_info(name,age,city):
+    print("Name:",name)
+    print("Age:",age)
+    print("City:",city)
+student_info(age=18,city="Rajkot",name="Ravi")    """
+
+
+"""def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=2)    """
+
+
+"""def simple_interest(p:float,r:int,t:float):
+    si=(p*r*t)
+    print("simple Tnterest:",si)
+simple_interest(p=10000,t=2,r=1.5)
+simple_interest(t=1.5,p=15000,r=2)"""
+
+
