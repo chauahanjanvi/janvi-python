c66644d5eeaeabcb759e6157e8d5b1ac5a56330f
chauahanjanvi <ck5282647@gmail.com>
2026-03-17 08:22:09 +0530

    Add files via upload

---
+++ argument.py
@@ -0,0 +1,56 @@
+"""def add(a,b):
+    print("a=",a)
+    print("b=",b)
+    return a+b
+result = add(2,5)
+print("sum=",result)"""
+
+
+"""def student_info (name,roll,marks):
+    print("Name:",name)
+    print("Rollno:",roll)
+    print("Marks:",marks)
+student_info ("Ravi",101,85)    """
+
+
+#simple interest
+"""def simple_interest(p,r,n):
+    si=(p*r*n)/100
+    print("simple Interest:",si)
+simple_interest(10000,2,2)
+simple_interest(5000,1.2,3) """
+
+
+"""def ar_circle(r):
+    a_circl=3.14*r*r
+    print("area of circle:",a_circl)
+ar_circle(1.5)
+ar_circle(4) """
+
+
+"""def check_value(no):
+    if(no>0):
+        print("positive")
+    elif(no<0):
+        print("negative")
+    else:
+        print("zero")
+check_value(0)
+check_value(90)
+check_value(-15)"""
+
+
+"""def odd_even(no):
+    if(no%2==0):
+        print(f"value{no}is even")
+    else:
+        print(f"value{no}is ODD")
+odd_even(50)
+odd_even(15)"""
+
+
+"""def addition(a,b):
+    add=a+b
+    print("addition of two values",add)
+addition(50,10.5)
+addition(100,200)    """
